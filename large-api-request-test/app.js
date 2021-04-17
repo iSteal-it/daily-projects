5af55e06ef0d7f09ce7459a897a62decf138d7a9
iSteal-it <47010892+iSteal-it@users.noreply.github.com>
2021-04-17 12:11:17 +0530

    Add files via upload

---
+++ large-api-request-test/app.js
@@ -0,0 +1,11 @@
+const https = require("https");
+
+https.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",function(response){
+  var data = "";
+  response.on("data",function(chunk){
+    data += chunk;
+  });
+  response.on("end",function(){
+    console.log(data);
+  });
+});
